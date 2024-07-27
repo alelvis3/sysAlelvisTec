5e49840a774a1bfbbadd950778f05293e1fcd07f
alelvis3 <alelv@AlexandreNote>
2024-07-27 09:41:49 -0300

    Spring Boot 3

---
+++ src/main/java/br/com/alelvis3/sysAlelvisTec/SysAlelvisTecApplication.java
@@ -0,0 +1,13 @@
+package br.com.alelvis3.sysAlelvisTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAlelvisTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysAlelvisTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alelvis3/sysAlelvisTec/SysAlelvisTecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alelvis3.sysAlelvisTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysAlelvisTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
